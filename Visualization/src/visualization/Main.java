9299a3592bf4c0f1868bb112d76c9845ad39e62e
gbasile001 <gaetan.basile.1@etu.u-bordeaux.fr>
2018-04-16 16:54:33 +0900

    main class documentation for clarity

--- Visualization/src/visualization/Main.java
+++ Visualization/src/visualization/Main.java
@@ -7,21 +7,34 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- *  By Gaetan Basile, Thomas Guesdon and Nathan Joubert
+ * Main entry point for the application.
+ *
+ * @author Nathan Joubert
+ * @author Thomas Guesdon
+ * @author Gaétan Basile
  */
-
 public class Main extends Application {
 
+    /**
+     * Main method
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Initializes the javaFX window and displays it.
+     *
+     * @param primaryStage the base window
+     * @throws Exception any exception
+     */
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("view/input.fxml"));
         primaryStage.setTitle(Tools.windowTitleBase);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
