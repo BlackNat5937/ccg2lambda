e2c95f58734ce5d9ac7dc382548db0679ffa1baa
tguesdon <thomas.guesdon12@gmail.com>
2018-04-17 15:38:36 +0900

    try of another type of graph

--- Visualization/src/visualization/controller/outputController.java
+++ Visualization/src/visualization/controller/outputController.java
@@ -33,9 +33,8 @@ public class outputController {
         listViewFormula.setItems(listFormulaItems);
 
         Graph g = new Graph();
-        g.getNodes().add(new Node("test", NodeType.EVENT));
         g.getNodes().add(new Node("test2",NodeType.NOUN));
-        g.getLinks().add(new Link(g.getNodes().get(0), g.getNodes().get(1), "is a"));
+        g.getNodes().add(new Node("test", NodeType.EVENT, g.getNodes().get(0), "is a"));
         System.out.println(g.toString());
     }
 
