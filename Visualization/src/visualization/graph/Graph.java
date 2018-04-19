7885cffd96df3ed7e7b3151f3de8809de39055c1
tguesdon <thomas.guesdon12@gmail.com>
2018-04-19 11:10:09 +0900

    draw arrow after the nodes

--- Visualization/src/visualization/graph/Graph.java
+++ Visualization/src/visualization/graph/Graph.java
@@ -32,30 +32,33 @@ public class Graph {
      * generate a canvas with the graphic representation of the graph
      * @return canvas
      */
-    public Canvas generateCanvas(){
-        Canvas c = new Canvas(400,400);
+    public Canvas generateCanvas() {
+        Canvas c = new Canvas(400, 400);
         GraphicsContext gc = c.getGraphicsContext2D();
         int cpt = 0;
 
-        for(Node n : nodes){
-            n.setX((cpt % 8)*100);
+        for (Node n : nodes) {
+            n.setX((cpt % 8) * 100);
             n.setY(20);
-            cpt ++;
+            cpt++;
         }
 
-        for(Node n : nodes){
+        for (Node n : nodes) {
             gc.setFill(Color.GREEN);
-            gc.fillRect(n.getX(), n.getY(), 20,20);
+            gc.fillRect(n.getX(), n.getY(), 20, 20);
             gc.setFill(Color.BLACK);
             gc.fillText(n.getLabel(), n.getX(), n.getY());
-            if(!n.getLinks().isEmpty()){
+
+        }
+        for (Node n : nodes) {
+            if (!n.getLinks().isEmpty()) {
                 gc.setStroke(Color.BLUE);
-                for(Link l : n.getLinks()){
-                    drawArrow(gc, n.getX() + 10,n.getY() + 10, l.getDestination().getX() + 10, l.getDestination().getY() + 10);
+                for (Link l : n.getLinks()) {
+                    drawArrow(gc, n.getX() + 10, n.getY() + 10, l.getDestination().getX() + 10, l.getDestination().getY() + 10);
                     //gc.strokeLine(n.getX() + 10,n.getY() + 10, l.getDestination().getX() + 10, l.getDestination().getY() + 10);
                     int midX = (l.getDestination().getX() + 10) - (n.getX() + 10);
                     int midY = (l.getDestination().getY() + 10) - (n.getY() + 10);
-                    gc.fillText(l.getText(), n.getX() + midX/2, n.getY() + midY/2);
+                    gc.fillText(l.getText(), n.getX() + midX / 2, n.getY() + midY / 2);
                 }
             }
         }
