c3ba927877b5a74f3462cda4c5dd16235f53046f
gbasile001 <gaetan.basile.1@etu.u-bordeaux.fr>
2018-04-19 17:33:43 +0900

    comments in BoxController for clarity

--- Visualization/src/visualization/controller/BoxController.java
+++ Visualization/src/visualization/controller/BoxController.java
@@ -10,23 +10,85 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+/**
+ * Controller for the view of a formula in DRS notation.
+ *
+ * @author Gaétan Basile
+ * @see visualization.view
+ * @see <a href="https://en.wikipedia.org/w/index.php?title=Discourse_representation_structure">DRS details</a>
+ */
 public class BoxController implements Parametrable<String> {
+    /**
+     * Pattern to match the declarations of variables.
+     */
+    private static final Pattern boxTokenMatcher =
+            Pattern.compile("\\(\\w+ = _\\)|_\\w+\\(((\\w+)|(\\w+,\\w+))\\)");
+    /**
+     * Pattern to match the declarations of variables including proper nouns.
+     */
+    private static final Pattern boxTokenMatcherBis =
+            Pattern.compile("\\(\\w+ = (_\\w+)\\)|_\\w+\\(((_\\w+)|(_\\w+,\\w+)|(\\w+,_\\w+))\\)");
+    /**
+     * Container for the content of the box.
+     */
     @FXML
     public VBox contentContainer;
+    /**
+     * Box structure. Contains the title.
+     */
     @FXML
     private TitledPane box;
-
-    private static final Pattern boxTokenMatcher = Pattern.compile("_\\w*\\(\\w\\)");
-
+    /**
+     * The formula output by ccg2lambda.
+     */
     private String formula;
+    /**
+     * The header of the box.
+     */
     private String header;
+    /**
+     * The content of the box, line by line.
+     */
     private List<String> tokens = new ArrayList<>();
 
+    /**
+     * Initializes the data for this box. Parses the formula, and creates content and header.
+     *
+     * @param formula the string of the formula
+     */
     public void initData(String formula) {
         this.formula = formula;
-        box.setText(formula);
         parseFormula();
 
+        createContent();
+        createHeader();
+    }
+
+    /**
+     * Parses the formula and creates the tokens.
+     */
+    private void parseFormula() {
+        Scanner sc = new Scanner(formula);
+        Scanner sc2 = new Scanner(formula);
+        sc.useDelimiter("&");
+        sc2.useDelimiter("&");
+        do {
+            String token, tokenBis;
+            token = sc.findInLine(boxTokenMatcher);
+            tokenBis = sc2.findInLine(boxTokenMatcherBis);
+            if (token != null)
+                tokens.add(token);
+            if (tokenBis != null)
+                tokens.add(tokenBis);
+            sc.next();
+            sc2.next();
+        } while (sc.hasNext() && sc.hasNext());
+    }
+
+    /**
+     * Creates the content of the box by using the tokens.
+     */
+    private void createContent() {
         for (String token : tokens) {
             if (token.length() > 1)
                 token = token.substring(1);
@@ -35,24 +97,15 @@ public class BoxController implements Parametrable<String> {
         }
     }
 
-    private List<String> parseFormula() {
-        Scanner sc = new Scanner(formula);
-        sc.useDelimiter("&");
-        do {
-            String token;
-            token = sc.findInLine(boxTokenMatcher);
-            if (token != null)
-                tokens.add(token);
-            sc.next();
-        } while (sc.hasNext());
-        return tokens;
-    }
-
-    private String createHeader() {
+    /**
+     * Creates the header of the box by using the tokens.
+     */
+    private void createHeader() {
         Pattern variableName = Pattern.compile("\\(\\w+\\)");
+        StringBuilder sb = new StringBuilder(header);
         List<String> used = new ArrayList<>();
         for (String token : tokens) {
+            sb.append(token);
         }
-        return header;
     }
 }
