6150ef0b0b1e4c4f028fdbac8b39c3fbdb670699
tguesdon <thomas.guesdon12@gmail.com>
2018-04-27 16:11:11 +0900

    Negation graphic class added

---
+++ Visualization/src/visualization/graph/Negation.java
@@ -0,0 +1,12 @@
+package visualization.graph;
+
+import java.util.ArrayList;
+
+public class Negation {
+
+    private ArrayList<Node> negated = new ArrayList<>();
+
+    public ArrayList<Node> getNegated() {
+        return negated;
+    }
+}
