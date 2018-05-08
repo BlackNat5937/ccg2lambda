799f277c2a39ac2247c83323f8a870efb01546df
tguesdon <thomas.guesdon12@gmail.com>
2018-05-08 15:48:38 +0900

    Disjunction : added two cases and fixed bug when 2 events

--- Visualization/src/visualization/utils/formula/ClassicParser.java
+++ Visualization/src/visualization/utils/formula/ClassicParser.java
@@ -183,13 +183,15 @@ public class ClassicParser extends BaseParser {
 
 
             //case PROG || Prog
-            if(containsProgArg(args1) && containsProgArg(args2)){
+            if(containsProgArg(args1) && containsProgArg(args2) && !containsConjArg(args1) && !containsConjArg(args2)){
                 Disjunction disjunction = new Disjunction();
                 String prog1 = getProgArg(args1);
                 String prog2 = getProgArg(args2);
                 System.out.println("Args : " + prog1 + " / " + prog2);
                 String name1 = prog1.substring(prog1.indexOf('_') + 1).substring(0,prog1.substring(prog1.indexOf('_') + 1).indexOf('('));
-                String name2 = prog2.substring(prog1.indexOf('_') + 1).substring(0,prog2.substring(prog2.indexOf('_') + 1).indexOf('('));
+                String name2 = prog2.substring(prog2.indexOf('_') + 1).substring(0,prog2.substring(prog2.indexOf('_') + 1).indexOf('('));
+
+                System.out.println("    Name : " + name1 + " & " + name2);
 
                 disjunction.setArg1(parseResult.getEvents().get(getEventKey(parseResult.getLambda().indexOf(name1), name1)));
                 disjunction.setArg2(parseResult.getEvents().get(getEventKey(parseResult.getLambda().indexOf(name2), name2)));
